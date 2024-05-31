3c099e39e24fdd5d182d416408e8f97e4a6b8647
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-05-31 12:12:17 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        lato: ["Lato"],
+        garamond: ["Garamond"],
+      },
+    },
+  },
+  plugins: [],
+};
