a699dd2174d18c7f35730112ae70548c8fccda24
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-06-10 16:13:17 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,14 @@ module.exports = {
     },
     colors: {
       myBlue: "#5863F8",
-      myBlack: "171D1C",
+      myBlack: "#171D1C",
+      myWhite: "#FFFFFF",
     },
     backgroundImage: {
-      "hero-background": "url('./src/img/image 9.png')",
+      heroBg: "url('../img/image 9.png')",
+      catMen: "url('../img/cat-men.png')",
+      catWomen: "url('../img/cat-women.png')",
+      catKids: "url('../img/cat-kids.png')",
     },
   },
   plugins: [],
